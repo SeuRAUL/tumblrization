6f855e9d54accb611e4e61a2feee20f782faf8e6
seuraul <seuraul.0@gmail.com>
2023-05-08 14:26:03 -0300

    Show Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,35 @@
 class PostsController < ApplicationController
+  before_action :post, only: [:edit, :update, :show, :destroy]
 
   def index
   end
 
   def new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
